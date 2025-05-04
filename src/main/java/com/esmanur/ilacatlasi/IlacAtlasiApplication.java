bda2f627efb90219a68eb4dac13d85971e72425c
Esma Nur Polat <esmanurpolat2723@gmail.com>
2025-05-04 22:10:56 +0300

    İlk commit: ilac-atlasi projesi başlatıldı

---
+++ src/main/java/com/esmanur/ilacatlasi/IlacAtlasiApplication.java
@@ -0,0 +1,13 @@
+package com.esmanur.ilacatlasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlacAtlasiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlacAtlasiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esmanur/ilacatlasi/IlacAtlasiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esmanur.ilacatlasi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlacAtlasiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
